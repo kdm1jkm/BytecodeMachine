9d7018cf1e18bc4931b389d973454c0ea369b10a
kdm <kdm1jkmm@gmail.com>
2021-08-06 11:46:27 +0900

    Add namespace std and complete main

--- src/main.cpp
+++ src/main.cpp
@@ -3,25 +3,71 @@
 #include <vector>
 #include <cstring>
 #include <set>
+#include <functional>
 #include "Machine.h"
+
+using namespace std;
 
 void printHelp(char *filename)
 {
-    std::cout << "Usage1: " << filename << " run <file> [param...]" << std::endl;
-    std::cout << "Usage2: " << filename << " compile <file> -o [output]" << std::endl;
+    cout << "Usage1: " << filename << " run <file> [param...]" << endl;
+    cout << "Usage2: " << filename << " compile <file> -o [output]" << endl;
+}
+
+string getInput(const set<string> &choices, const function<void()> &runBeforeAsk)
+{
+    string input;
+    while (true)
+    {
+        runBeforeAsk();
+        getline(cin, input);
+        if (choices.find(input) != choices.end())
+            break;
+    }
+
+    return input;
 }
 
 int main(int argc, char **argv)
 {
     if (argc == 1)
     {
-        std::string input = "";
-        const std::set<std::string> choices = {"1", "2"};
-        while (choices.find(input) == choices.end())
+        string choice = getInput({"1", "2"}, []
+                                 { cout << "1. run" << endl
+                                        << "2. compile" << endl
+                                        << "Enter>>"; });
+
+        string filename;
+        cout << "Enter file>>";
+        cin >> filename;
+
+        switch (stoi(choice))
         {
-            std::cout << "1. run" << std::endl;
-            std::cout << "2. compile" << std::endl;
-            std::cin >> input;
+        case 1:
+        {
+            vector<int32_t> param;
+            string input;
+
+            cin.ignore();
+            while (true)
+            {
+                cout << "Enter param[" << param.size() << "]>>";
+                getline(cin, input);
+                if (input == "")
+                    break;
+
+                param.push_back(stoi(input));
+            }
+
+            Machine machine(filename, param);
+            machine.runUntilEnd();
+            return EXIT_SUCCESS;
+        }
+        case 2:
+        {
+            cout << "Not supported" << endl;
+            return EXIT_SUCCESS;
+        }
         }
     }
 
@@ -31,13 +77,21 @@ int main(int argc, char **argv)
     }
     else if (strcmp(argv[1], "run") == 0)
     {
-        std::vector<int32_t> param;
+        vector<int32_t> param;
         for (int i = 3; i < argc; i++)
         {
-            param.push_back(std::stoi(argv[i]));
+            param.push_back(stoi(argv[i]));
         }
         Machine machine(argv[2], param);
         machine.runUntilEnd();
+    }
+    else if (strcmp(argv[1], "compile") == 0)
+    {
+        cout << "Not supported" << endl;
+    }
+    else
+    {
+        printHelp(argv[0]);
     }
 
     return EXIT_SUCCESS;
